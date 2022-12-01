c96c3b0937ed4bb0084d38d50b939c53fd4baf71
jagadeesanmay7 <113778854+jagadeesanmay7@users.noreply.github.com>
2022-12-02 01:23:20 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,28 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  //interpolation
   public Whoami = 'Angular';
+  //property binding
   public isDisabled = true;
+  //class binding
+  isSucess = true;
+  isSpecial = true;
+  specialclass = 'text-special';
+  public messageClasses = {
+    'text-danger': !this.isSucess,
+    'text-success': this.isSucess,
+    'text-special': this.isSpecial,
+  };
+  //event binding
+  public greetUser = '';
+  onClick() {
+    this.greetUser = 'Welcome to Angular';
+  }
+  //Template reference variable
+  public TRV = '';
+  onClick_TRV(value) {
+    this.TRV = value;
+  }
+  public sync = '';
 }
